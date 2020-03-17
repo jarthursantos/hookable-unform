710e7d40a6bfb267432ade025b691541d9a4aee0
jarthursantos <silva.arthur@academico.ifpb.edu.br>
2020-03-17 08:33:16 -0300

    feat: form validation

---
+++ src/index.ts
@@ -0,0 +1,46 @@
+import { RefObject, useCallback } from "react";
+
+import { FormHandles } from "@unform/core";
+import { ObjectSchema, ValidationError } from "yup";
+
+interface ValidationResult {
+  success: boolean;
+  errors?: { [key: string]: string };
+  data?: object;
+}
+
+export function useFormValidator(
+  ref: RefObject<FormHandles>,
+  schema: ObjectSchema
+) {
+  return useCallback(
+    async function handle(): Promise<ValidationResult> {
+      if (!ref.current) throw Error("null form reference");
+
+      const data = ref.current.getData();
+
+      try {
+        await schema.validate(data, {
+          abortEarly: false
+        });
+
+        return { success: true, data };
+      } catch (err) {
+        const validationErrors: { [key: string]: string } = {};
+
+        if (err instanceof ValidationError) {
+          err.inner.forEach(error => {
+            validationErrors[error.path] = error.message;
+          });
+
+          ref.current.setErrors(validationErrors);
+
+          return { success: false, errors: validationErrors };
+        }
+
+        throw err;
+      }
+    },
+    [ref, schema]
+  );
+}
